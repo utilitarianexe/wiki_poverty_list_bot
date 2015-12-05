456c8e7eecbe7edc72ab6f358984c5696ec682f7
Peter <utilitarianexe@gmail.com>
2015-12-04 18:40:51 -0800

    lots of code clean up needed but we now print out the complete first table for the page

--- create_table.py
+++ create_table.py
@@ -1,86 +1,205 @@
 '''
-update to use https://pypi.python.org/pypi/wbpy/2.0.1
 dev things----------
-need requirments file
+update to use https://pypi.python.org/pypi/wbpy/2.0.1
 we need tests
 need to handle all error conditions
-we need python tools(autocomplete, sytax checker, pep8, debugger)
+make sure all pylint is good
 read all doc strings
 code review
 copy paste
 redundant code
 bad naming
 
-problems at current feature set -------------
-how do we decide what years to consider
-how do we quotes around names
-how do we fix
+manaul update work --------------
+remove bangladesh citation
+remove gambia citation
+change description of chart
+look at notes about wiki bots
 
-required features--------------
-get numbers for all income levels
-why so few countries in data sets
-remove non country entities or find some way to deal with them
-deal with quotes
-
+like to have------------------
 auto posting
+updating the image
+cia data
+compain to world bank about incomplete data
+auto fill in dates for access times
 auto pulling
 other tables
-updating the image
 informing other editors
 '''
 
-import world_bank_csv
 import world_bank_api
 import iso3166 # country code data
+import datetime
 
-def build_wiki_table(table_data):
+
+def build_table_text(table_data):
+    #December 4, 2015
+    now = datetime.date.today()
+    
+    header = '''{{| class="wikitable sortable" style="margin-left:auto;margin-right:auto;text-align: right"
+|-
+! data-sort-type = text | Country
+! data-sort-type = number | $1.90<ref name="wdipov">{{{{cite web | url =http://data.worldbank.org/indicator/SI.POV.2DAY | title =World Development Indicators | publisher =[[World Bank]] | date = {date1}  | accessdate = {date2} }}}}</ref>
+! data-sort-type = number | Year
+! data-sort-type = number | < $3.10<ref name="wdipov">{{{{cite web | url =http://data.worldbank.org/indicator/SI.POV.2DAY | title =World Development Indicators | publisher =[[World Bank]] | date = {date3} | accessdate = {date4} }}}}</ref>
+! data-sort-type = number | Year
+|-'''
+    date_text = now.isoformat()
+    header = header.format(date1=date_text, date2=date_text, date3=date_text, date4=date_text)
+    footer = '|}'
+    rows = build_table_rows(table_data)
+    rows = rows[:-4] # remove final row seperator
+    table_text = '{header}\n{rows}\n{footer}'.format(header=header, rows=rows, footer=footer)
+    return table_text
+
+
+def reference_overwrite(name, rate_190, year_190, rate_310, year_310):
+    india_ref = '<ref>http://pubdocs.worldbank.org/pubdocs/publicdoc/2015/10/109701443800596288/PRN03-Oct2015-TwinGoals.pdf</ref>'
+    overwrite_dict = {'India': {'level_190': {'year': '2012', 'rate':'12.4', 'ref': india_ref},
+                                'level_310': None}}
+    ref_190 = None
+    ref_310 = None
+    if name not in overwrite_dict:
+        return rate_190, year_190, ref_190, rate_310, year_310, ref_310
+
+    overwrite = overwrite_dict[name]
+    if overwrite['level_190'] is not None:
+        if int(overwrite['level_190']['year']) > int(year_190):
+            year_190 = overwrite['level_190']['year']
+            rate_190 = overwrite['level_190']['rate']
+            ref_190 = overwrite['level_190']['ref']
+
+    if overwrite['level_310'] is not None:
+        if int(overwrite['level_310']['year']) > int(year_310):
+            year_310 = overwrite['level_310']['year']
+            rate_310 = overwrite['level_310']['rate']
+            ref_310 = overwrite['level_310']['ref']
+
+            
+    return rate_190, year_190, ref_190, rate_310, year_310, ref_310
+
+
+def remap_country(name):
+    '''
+    Many countries returned by the world bank api do match the wikipedia flag template names
+    this funcion manually remaps them
+    '''
+    flag_name_by_world_bank_name = {
+        'Virgin Islands (U.S.)': 'United States Virgin Islands',
+        'Egypt, Arab Rep.': 'Egypt',
+        'Hong Kong SAR, China': 'Hong Kong',
+        'Korea, Dem. Rep.': 'North Korea',
+        'Lao PDR': 'Laos',
+        'Macao SAR, China': 'Macao',
+        'St. Martin (French part)': 'Collectivity of Saint Martin',
+        'Venezuela, RB': 'Venezuela',
+        'Yemen, Rep.': 'Yemen',}
+    return flag_name_by_world_bank_name.get(name, name)
+
+
+def build_table_rows(table_data):
     '''
     ---input
-    see outpute of parse_poverty_data
+    see output of world_bank_api.most_recent_poverty_data
     ---output
     |-
     | style="text-align:left;"| {{flag|Albania}} || 0.5 || 3.00 || 2012 || 31.5 || 2012
     |-
     | style="text-align:left;"| {{flag|Algeria}} || 6.79 || 23.61 || 1995 || 63 || 1995
     |-
-    --todo
-    what happens if we are missing a 
+    flag problemsdd
+    Virgin Islands (U.S.) -> United States Virgin Islands
+    Egypt, Arab Rep. -> Egypt
+    Hong Kong SAR, China -> Hong Kong
+     Korea, Dem. Rep. -> North Korea
+    Lao PDR -> Laos
+    Macao SAR, China -> Macao
+    St. Martin (French part) -> Collectivity of Saint Martin
+    Venezuela, RB -> Venezuela
+    Yemen, Rep. -> Yemen
     '''
-    table_text = ''
     row_texts = []
-    for country_id, country_data in table_data.items():
-        row_break = '|-\n'
+    number_valid = 0
+    countries_with_bad_data_190 = set()
+    countries_with_bad_data_310 = set()
+    countries_good = set()
+    for country_data in table_data:
+        country_id = country_data['country_code']
         name = country_data['country_name']
-        year = country_data['most_recent_date']
-        rate = country_data['percent']
-        print(country_id, name)
-        if year is None or rate is None:
-            print('missing data for: ' + name)
+        name = remap_country(name)
+        try:
+            iso3166.countries.get(country_id)
+        except KeyError:
+            message = 'non country code {} name {} skipping these are usually regional data'
+            message = message.format(country_id, name)
+            print(message)
             continue
 
-        # TODO simplified take a look at doc string for whats currently on page
-        # not using standard  python string formating because of conflicts with markup
-        row_text = '| style="text-align:left;"| {{flag|' + name + '}} ||' + rate + ' || ' + year + '\n'
-        row_text = row_text + row_break
+        year_310 = country_data['poverty_rates']['3.10']['most_recent_date']
+        rate_310 = country_data['poverty_rates']['3.10']['percent']
+        year_190 = country_data['poverty_rates']['1.90']['most_recent_date']
+        rate_190 = country_data['poverty_rates']['1.90']['percent']
+
+        if year_310 is None or rate_310 is None or rate_310 == '0':
+            countries_with_bad_data_310.add(name)
+            year_310 = '{{N/A}}'
+            rate_310 = '{{N/A}}'
+
+        if year_190 is None or rate_190 is None or rate_190 == '0':
+            countries_with_bad_data_190.add(name)
+            year_190 = '{{N/A}}'
+            rate_190 = '{{N/A}}'
+
+        rate_190, year_190, ref_190, rate_310, year_310, ref_310 = reference_overwrite(name, rate_190, year_190, rate_310, year_310)
+        row_text = build_row_text(name, rate_190, year_190, rate_310, year_310,
+                                  extra_reference_190=ref_190,
+                                  extra_reference_310=ref_310)
+        number_valid += 1
+        countries_good.add(name)
         row_texts.append(row_text)
-    
-    table_text = ''.join(row_texts)
-    return table_text
+
+    rows_text = ''.join(row_texts)
+    #print(countries_with_bad_data_190.symmetric_difference(countries_with_bad_data_310))
+    # print(old_countries.difference(countries_good))
+    # print(len(old_countries))
+    # print(len(countries_good))
+    return rows_text
+
+
+def build_row_text(name, rate_190, year_190, rate_310, year_310,
+                   extra_reference_190='',
+                   extra_reference_310=''):
+    if extra_reference_190 is None:
+        extra_reference_190 = ''
+    if extra_reference_310 is None:
+        extra_reference_310 = ''
+    # not using standard  python string formating because of conflicts with markup
+    row_break = '|-\n'
+    text_190 = rate_190 + extra_reference_190 + ' || ' + year_190
+    text_310 = rate_310 + extra_reference_310 + ' || ' + year_310
+    rate_year_info_text = text_190 + ' || ' + text_310
+    country_text = '| style="text-align:left;"| {{flag|' + name + '}} ||'
+    row_text = country_text + rate_year_info_text + '\n'
+    row_text = row_text + row_break
+    return row_text
 
 
 def post_table(wiki_table):
-    # api if possible otherwise got to hack it or maybe just print the full edit for now
-    print('not implemented you need to paste the folowing yourself')
-    print(wiki_table)
+    '''
+    current outputs to file might be cool to write direct to page
+    '''
+    output_file = open('output_markup', 'w')
+    output_file.write(wiki_table)
+    output_file.close()
+    print('markup written to output_markup')
 
 
 def build_table():
-    #poverty_data = world_bank_csv.most_recent_poverty_data()
     poverty_data, error = world_bank_api.most_recent_poverty_data()
     if error is not None:
         print(error)
         return
-    wiki_table = build_wiki_table(poverty_data)
+    wiki_table = build_table_text(poverty_data)
     post_table(wiki_table)
 
 
--- world_bank_api.py
+++ world_bank_api.py
@@ -16,6 +16,7 @@ def generic_query(uri, params={}):
     info = []
     page_number = 1
     while True:
+
         result = requests.get(uri, params=params)
         if result.status_code != 200:
             return None, 'non 200 response'
@@ -30,16 +31,32 @@ def generic_query(uri, params={}):
     return info, None
 
 def poverty_data():
-    uri = 'countries/all/indicators/SI.POV.NOP2'
+    indicator_to_data = {}
+    # this one does 2011 PPP for percentage of population < 1.90
+    indicator = 'SI.POV.DDAY'
+    uri = 'countries/all/indicators/' + indicator
     data, error = generic_query(uri)
-    return data, error
+    if error is not None:
+        return None, error
+    indicator_to_data['1.90'] = data
 
-def query_country(country_code):
+    # this one does 2011 PPP for percentage of population < 3.10
+    indicator = 'SI.POV.2DAY'
+    uri = 'countries/all/indicators/' + indicator
+    data, error = generic_query(uri)
+    if error is not None:
+        return None, error
+    indicator_to_data['3.10'] = data
+
+    return indicator_to_data, error
+
+
+def query_country(country_code, indicator='1.90'):
     data, error = poverty_data()
-    for row in data:
+    for row in data[indicator]:
         if row['country']['id'] == country_code:
             print(row)
-    
+
 
 def parse_poverty_data(raw_data):
     '''
@@ -48,10 +65,9 @@ def parse_poverty_data(raw_data):
      {'decimal': '0', 'value': None, 'indicator': {'id': 'SI.POV.NOP2', 'value': 'Number of poor at $3.10 a day (2011 PPP) (millions)'}, 'country': {'id': 'ZW', 'value': 'Zimbabwe'}, 'date': '1990'},
 
     output ---------------------
-    dictionary of dictionaries with key being country id
+    array of dictionaries
     the sub dicts look like
-    {'country_name':Zimbabwe, 'most_recent_date':1990, percent:10.1}
-
+    {'country_name':'Zimbabwe', 'most_recent_date':'1990', 'percent':10.1, 'country_code':'ZW'}
     if no data can be found percent will be None
 
     issues -----------------
@@ -60,34 +76,53 @@ def parse_poverty_data(raw_data):
     we need to scrub regions out or do something with them
     '''
     country_id_to_poverty_data = {}
-    for row in raw_data:
-        country_id = row['country']['id']
-        if country_id not in country_id_to_poverty_data:
-            country_id_to_poverty_data[country_id] = {'country_name': row['country']['value'],
-                                                      'most_recent_date': None,
-                                                      'percent': None}
-        country_poverty_data = country_id_to_poverty_data[country_id]
-        most_recent_data_date = country_poverty_data['most_recent_date']
+    for indicator, data in  raw_data.items():
+        for row in data:
+            country_id = row['country']['id']
+            if country_id not in country_id_to_poverty_data:
+                country_id_to_poverty_data[country_id] = {'country_name': row['country']['value'],
+                                                          'poverty_rates':{}}
+            country_poverty_data = country_id_to_poverty_data[country_id]
+            if indicator not in country_poverty_data['poverty_rates']:
+                country_poverty_data['poverty_rates'][indicator] = {'most_recent_date': None,
+                                                                    'percent': None}
 
-        if row['value'] is None or row['date'] is None:
-            continue
+            country_indicator_poverty_data = country_poverty_data['poverty_rates'][indicator]
 
-        if most_recent_data_date is None or int(row['date']) > int(most_recent_data_date):
-            country_poverty_data['percent'] = row['value']
-            country_poverty_data['most_recent_date'] = row['date']
+            most_recent_data_date = country_indicator_poverty_data['most_recent_date']
 
-    return country_id_to_poverty_data
-        
+            if row['value'] is None or row['date'] is None:
+                continue
+
+            if most_recent_data_date is None or int(row['date']) > int(most_recent_data_date):
+                country_indicator_poverty_data['percent'] = row['value']
+                country_indicator_poverty_data['most_recent_date'] = row['date']
+
+    poverty_data_for_countries = listify_dict(country_id_to_poverty_data)
+    return sorted(poverty_data_for_countries, key=lambda country_data: country_data['country_name'])
+
+
+def listify_dict(data, id_name='country_code'):
+    '''
+    go from a dict of dicts by id
+    to an array with each dict and id added to the inner dicts
+    '''
+    items = []
+    for k, item in data.items():
+        item[id_name] = k
+        items.append(item)
+    return items
+
 
 def most_recent_poverty_data():
     data, error = poverty_data()
     if error is not None:
         return data, 'could not get data on poverty from world bank: ' + error
-    
+
     data = parse_poverty_data(data)
     return data, None
-        
+
 
 if __name__ == '__main__':
     #print(most_recent_poverty_data())
-    query_country('DZ')
+    query_country('NG')
